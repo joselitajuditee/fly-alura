7351bb6cfa1894dd44221babe248f6aa2d1a9646
Joselita Judite <joselitajuditedesouza@gmail.com>
2025-01-15 20:30:03 -0300

    Adding files to first logic lessons

---
+++ Lógica de Programação/projeto-inicial-js-logica/app.js
@@ -0,0 +1,211 @@
+alert('Olá Mundo');
+
+alert('Boas-vindas ao jogo do numero secreto');
+let numeroSecreto = 5;
+
+let chute = prompt('Escolha um numero entre 1 e 10');
+if(numeroSecreto == chute){
+    console.log ('Isso aí você descobriu o numeroSecreto'(5)) 
+}
+
+let mensagemDeBoasVindas = 'Boas vindas ao App';
+alert('mensagemDeBoasVindas');
+
+alert('Boas-vindas ao jogo do número secreto')
+let chute = prompt('Escolha um número entre 1 e 10')
+
+let numeroSecreto = 4
+
+if (chute == numeroSecreto) {
+   alert('Acertou')
+}
+
+let senhaDoSistema = "senhaTeste!";
+
+let senha = prompt("Digite a senha do sistema:");
+
+if (senha = senhaDoSistema){
+    alert("Acesso ao sistema garantido");
+}
+alert('Boas-vindas ao jogo do número secreto');
+let numeroSecreto = 29;
+
+alert('Boas-vindas ao jogo do número secreto');
+let numeroSecreto = 29;
+
+prompt('Escolha um número 
+    entre 1 e 30');
+    alert('Boas-vindas ao jogo do número secreto');
+    let numeroSecreto = 5;
+    prompt('Escolha um número entre 1 e 10');
+    alert('Boas-vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+        console.log("Isso aí! você descobriu o número secreto (5)");
+}
+let mensagemDeBoasVindas = 'Boas vindas ao App';
+alert('mensagemDeBoasVindas');
+alert('Boas-vindas ao jogo do número secreto')
+let chute = prompt('Escolha um número entre 1 e 10')
+
+let numeroSecreto = 4
+
+if (chute == numeroSecreto) {
+   alert('Acertou')
+}
+
+let senhaDoSistema = "senhaTeste!";
+
+let senha = prompt("Digite a senha do sistema:");
+
+if (senha = senhaDoSistema){
+    alert("Acesso ao sistema garantido");
+}"Boas vindas ao nosso site!"
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+
+let saldoDisponivel = 1000;
+alert ("Erro! Preencha todos os campos")
+console.error(mensagemDeErro);
+let nome = prompt("digite o seu nome");
+let idade = prompt ("Digite a sua idade");
+if (idade>= 18){
+   console.log("Você é maior de idade."); alert("Pode tirar a habilitação!");
+} else {
+    console.log("Você é menor de idade.");
+
+}
+let contador = 1;
+while(contador <= 10){
+    console.log(contador);
+    contador++;
+}
+let contador = 10;
+while (contador.= 0);{
+    console.log(contador
+    contador--;
+} 
+let numeroMaximo = prompt("digita um número para a contagem regressiva:")
+while (numeroMaximo >= 0){console.log(numeroMaximo);
+    numeroMaximo--;
+}
+let numeroMaximo = prompt("Digite um numero para a contagem progressiva:");;
+let contador = 0;
+while (contador <= numeroMaximo){
+    console.log(contador);
+        contador++
+    
+}
+let idade = 25;
+let possuiCarteira = true;
+if (idade > 18 && possuiCarteira){
+    console.log("pode dirigir");
+
+} else{
+    console.log("Não pode dirigir.");
+}
+let temMaça = FontFaceSetLoadEvent;
+let temBanana = true;
+if (temMaça || temBanana){
+    console.log("Você tem frutas");{
+    } else { 
+      console.log("Não tem frutas");
+
+    }
+}let palavraPessoa = "";
+if(quantidadePessoas == 1){
+    palavraPessoa = "pessoa";
+}else{
+    palavraPessoa = "pessoas"
+}
+console.log("boas vindas");
+let nome = "Joselita Judite";
+console.log ('Olá, ${none}!');
+let nome = 'Joselita Judite';
+alert('Olá, ${nome}!');
+let linguagemPerfeita = prompt('Qual a lingagemPreferida';
+    let valor1 = 42;
+    let valor2 = 8;
+    let resultado = valor1 + valor2;
+    console.log('A soma de ${valor1} e #{valor2} é igual a ${resultado}.')
+)
+let idade = prompt('digite a sua idade:');
+if(idade > 17 ){
+    console.log('Você é maior de idade:');
+} else {
+    console.log('Você é menor de idade.');
+}
+var numero = parseFloat(prompt("digite um número"));
+    if (número > 0 ){
+        console.log("O número é positivo.");
+        else if(numero < 0){
+            console.log("O nùmero é negativo.");
+            else {
+                console.log("O número é zero.");
+                let numero  = 1;
+                while(número <= 10){
+                    console.log(Número);
+                    número++;
+        }
+        let nota = 8 
+        if (nota >= 7){
+            console.log("Aprovado");
+        } else {
+            console.log("Reprovado");
+        }
+
+        let nomeroAleatorio = Match.random();
+        console.log(numeroAleatorio); 
+        
+let numeroAleatorio = parseInt(Math.random() * 10) + 1;
+console.log(numeroInteiroAleatorio);
+let numeroInteiroAleatorio = parseInt(Math.random() * 10) + 1;
+console.log(numeroInteiroAliatirio);
+
+let numeroInteiroAliatorio = parseInt(Math.random() * 10) + 1;
+console.log(numeroInteiroAliatorio);
+let numeroInteiroAliatorio = parseInt(Math.random() = 1000) + 1;
+console.log(numeroInteiroAleatorio
+
+);
+alert('Bem-vindo ao nosso site!');
+let nome = "Lua";
+let idade = 25
+let numetoDeVendas = 50;
+let saldodesponivel = 1000
+alert('Erro! Preencha todos os campos');
+let nome = prompet ("digite seu nome")
+let idade = prompt('Digite a sua idade');
+let idade = prompt('Digite a sua idade');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!'); 
+}
+// HTML 
+ <p>Jogador 1: Chris</p>
+// CSS 
+p {
+    font-family: "helvetica neue", helvetica, sans-serif;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    text-align: center;
+    border: 2px solid rgba(0, 0, 200, 0.6);
+    background: rgba(0, 0, 200, 0.3);
+    color: rgba(0, 0, 200, 0.6);
+    box-shadow: 1px 1px 2px rgba(0, 0, 200, 0.4);
+    border-radius: 10px;
+    padding: 3px 10px;
+    display: inline-block;
+    cursor: pointer;
+  }
+  // javascript
+  const para = document.querySelector("p");
+
+para.addEventListener("click", atualizarNome);
+
+function atualizarNome() {
+  var nome = prompt("Insira um novo nome");
+  para.textContent = "Jogador 1: " + nome;
+}
